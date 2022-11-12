899c1c13152605a06ae5527aadd184cd9bea3aca
Volkov <volkov.aliksandr0203@gmail.com>
2022-11-12 13:53:15 +0200

    Changed messege in last two lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,5 +3,5 @@ Console.WriteLine("Volkov");
 Console.WriteLine("Alexander");
 Console.WriteLine("Maximovich");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("ZDTU");
+Console.WriteLine("IPZ - 21 - 5");
